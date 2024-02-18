58ac7838190e53414858a95ada7c1f325097cde0
dannyteofilo <danyel.285309@gmail.com>
2024-02-18 17:25:24 -0600

    feat: header component created

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        'custom-red': '#D00B2C',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
